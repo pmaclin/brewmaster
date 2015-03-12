f415741332154a55b19a9253d78efd51772bbe01
pmaclin <pcharlesantonio@gmail.com>
2015-03-12 10:55:03 -0500

    Adds association methods

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,4 +1,7 @@
 class Batch < ActiveRecord::Base
   belongs_to :beer_style
   belongs_to :recipe
+
+  has_many :reviews
+  has_many :uniques
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
+
+  has_many :batches
 end
